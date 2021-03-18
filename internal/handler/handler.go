096d7456d19439531a1fbe56e14da152d863359b
JavaHutt <alxmer111@gmail.com>
2021-03-18 17:31:32 +0400

    rest api routes

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/JavaHutt/rest-dealership/internal/service"
@@ -19,8 +20,39 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h Handler) InitRoutes() *chi.Mux {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.Route("/api/v1/vehicles", func(r chi.Router) {
+		r.Get("/", h.getAllVehicles)
+		r.Get("/{id}", h.getVehicleById)
+		r.Post("/", h.createVehicle)
+		r.Put("/", h.updateVehicle)
+		r.Delete("/", h.deleteVehicle)
 	})
 	return r
+}
+
+func (h Handler) getAllVehicles(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("getAll method"))
+}
+
+func (h Handler) getVehicleById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	res := fmt.Sprintf("get by id: %s", id)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(res))
+}
+
+func (h Handler) createVehicle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("create"))
+}
+
+func (h Handler) updateVehicle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("update"))
+}
+
+func (h Handler) deleteVehicle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
+	w.Write([]byte("delete"))
 }
