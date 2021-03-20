7d56482827a5a858351dfcfee05195263d2e5ba7
JavaHutt <alxmer111@gmail.com>
2021-03-20 13:52:12 +0400

    graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/JavaHutt/rest-dealership/internal/action"
 	"github.com/JavaHutt/rest-dealership/internal/handler"
@@ -45,8 +48,20 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(action.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("Error occured while starting http server: %s", err.Error())
+		}
+	}()
+	logrus.Print("Rest dealership server started...")
 
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("Error occured while starting http server: %s", err.Error())
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Shutting down rest dealership server...")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Fatalf("Error occured while shutting down server: %s", err.Error())
 	}
 }
